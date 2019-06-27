8a07fcb7dc6fc2120eb506c13177bcbd8f82b170
oboodalui <40316050+oboodalui@users.noreply.github.com>
2019-06-27 21:29:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "582345121442299934") return;
+  if (message.author.id !== "579442499291054090") return;
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
